a9f7a4714e312e381c95afbc7943321d7eb13e23
KarolTalaga10 <karol74fcb@interia.pl>
2021-01-19 16:49:20 +0100

    Finish app

--- src/sa_implementation.py
+++ src/sa_implementation.py
@@ -1,9 +1,9 @@
 import numpy as np
+import streamlit as st
 from data_prep import EVENTS, FUEL_COST, NAMES
 from enum import Enum
 import matplotlib.pyplot as plt
 import random
-import sys
 from copy import deepcopy
 
 
@@ -230,6 +230,81 @@ class Solution:
         plt.show()
 
 
+class SolutionApp(Solution):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def print_solution(solution):
+        for event in solution:
+            st.write('* {0}, {1}, {2}'.format(event.name, event.time_start, event.time_stay))
+
+    def sym_ann_algorithm(self):
+        T = self.T_init
+        current_solution = deepcopy(self.sol_init)
+        current_profit = self.calculate_max_profit(current_solution)
+        worst_profit = current_profit
+        i = 1
+        num_of_iter = 1
+        while T > self.T_min:
+            i += 1
+            for j in range(self.chain_len):
+                neighbour = deepcopy(self.get_neighbour(current_solution))
+                neighbour_profit = self.calculate_max_profit(neighbour)
+                if neighbour_profit > current_profit:
+                    # Accept new solution (better than previous best)
+                    current_solution = deepcopy(neighbour)
+                    current_profit = neighbour_profit
+                    if current_profit > self.best_profit:
+                        self.best_profit = current_profit
+                        self.best_solution = deepcopy(current_solution)
+                        num_of_iter = i
+                    if neighbour_profit < worst_profit:
+                        worst_profit = neighbour_profit
+
+                else:
+                    acc = np.random.uniform(0, 1)
+                    if acc < np.exp((neighbour_profit - current_profit) / T):
+                        # Accept worse solution with probability acc
+                        current_solution = deepcopy(neighbour)
+                        current_profit = neighbour_profit
+
+            T = self.get_temp(i)
+            self.T_list.append(T)
+            self.acc_list.append(np.exp((neighbour_profit - current_profit) / T))
+            self.profit_list.append(current_profit)
+        st.subheader('Your final solution is: ')
+        SolutionApp.print_solution(self.best_solution)
+        st.subheader('Best profit is:')
+        st.write('Best profit: ', self.best_profit)
+        st.write('Found on {0} iteration'.format(num_of_iter))
+
+        # Plot graphs
+        fig3 = plt.figure(figsize=(15, 5))
+        ax2_3 = fig3.add_subplot(131)
+        ax3_3 = fig3.add_subplot(132)
+        ax4_3 = fig3.add_subplot(133)
+
+        # Profit plot
+        ax2_3.axis([0, len(self.T_list), worst_profit - 2000, self.best_profit + 2000])
+        ax2_3.plot(np.linspace(0, len(self.T_list), num=len(self.T_list), endpoint=False), self.profit_list,
+                   linewidth=0.5, color='darkgreen')
+        ax2_3.set_title('Profit graph')
+
+        # Acceptance plot
+        ax3_3.axis([0, len(self.T_list), -0.1, 1.1])
+        ax3_3.scatter(np.linspace(0, len(self.T_list), num=len(self.T_list), endpoint=False), self.acc_list, s=1.0,
+                      color='darkgreen')
+        ax3_3.set_title('Acceptance graph')
+
+        # Temperature plot
+        ax4_3.axis([0, len(self.T_list), 0, self.T_init])
+        ax4_3.plot(np.linspace(0, len(self.T_list), len(self.T_list), endpoint=False), self.T_list, linewidth=1.0,
+                   color='darkgreen')
+        ax4_3.set_title('Temperature graph')
+        st.write(fig3)
+
+
 def generate_init_route(num_events: int, num_days: int) -> list:
     init = []
     dividers = sorted(random.sample(range(1, num_days - num_events), num_events - 1))
@@ -259,9 +334,10 @@ if __name__ == '__main__':
     init_route = generate_init_route(5)
     init_solution = Solution(init_route, 2200, SolMethod.GEO, EVENTSt)
     init_solution.sym_ann_algorithm()
-    '''
+    
     # sys.stdout = open("test.txt", "w")
-    init_route = generate_init_route(6, 31)
-    final_solution = Solution(init_route, 5000, 0.999, 10, SolMethod.GEO, 20, init_route, 0)
-    final_solution.sym_ann_algorithm()
+    #init_route = generate_init_route(6, 31)
+    #final_solution = Solution(init_route, 5000, 0.999, 10, SolMethod.GEO, 20, init_route, 0)
+    #final_solution.sym_ann_algorithm()
     # sys.stdout.close()
+    '''
--- src/stream.py
+++ src/stream.py
@@ -1,36 +1,67 @@
 import streamlit as st
-from sa_implementation import Event, Solution, generate_init_route
-# from PIL import Image
+from sa_implementation import Event, SolutionApp, SolMethod, generate_init_route
+from data_prep import NAMES
+from PIL import Image
+import base64
+
 st.title('Find best path for Food - Truck!')
-# img = Image.open('../images/ft.png')
-# st.image(img, width=None)
-
-
-def print_solution(solution):
-    st.write('Your initial solution is: ')
-    for event in solution:
-        st.write('* {0}, {1}, {2}'.format(event.name, event.time_start, event.time_stay))
-
+file_ = open("../images/loading.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+img = Image.open('../images/ft2.jpg')
+start = st.image(img, width=500)
 
 st.sidebar.subheader('Set parameters')
-st.sidebar.selectbox('Alpha', (0.999, 0.99, 0.9))
+params = {'alpha': st.sidebar.selectbox('Alpha', (0.999, 0.99, 0.9))}
 T_init = st.sidebar.slider('Initial temperature', 0, 5000, 0, step=10)
-T_stop = st.sidebar.slider("Stop temperature", 0, 500, 0, step=1)
+T_stop = st.sidebar.slider("Stop temperature", 0, 100, 0, step=1)
 chain_len = st.sidebar.slider('Length of Markov chain', 0, 100, 0, step=5)
-num_days = st.sidebar.slider('Number of cycle duration', 1, 31, 1, step=1)
-num_events = st.sidebar.slider('Number of events hosts', 1, 8, 1, step=1)
 init_select = st.sidebar.selectbox(
     "How would you like to create initial solution?",
-    ("Randomly", "Manually")
+    ('-', "Randomly", "Manually")
 )
 
 if init_select == 'Randomly':
+    num_events = st.sidebar.slider('Number of events hosts', 1, 8, 1, step=1)
+    num_days = st.sidebar.slider('Number of cycle duration', num_events * 2, 31, num_events * 2, step=1)
     init_route = generate_init_route(num_events, num_days)
+
+elif init_select == 'Manually':
+    num_events = st.sidebar.slider('Number of events hosts', 1, 8, 1, step=1)
+    sum_days = 0
+    init_route = []
+    day_of_selling = []
+    day_start = 1
+    for i in range(num_events):
+        names = {'events': st.selectbox('{0} event name '.format(i + 1), NAMES)}
+        day_of_selling.append(
+            st.slider("How many days do you want to be in {0} event?".format(i + 1), 1, 31, 1, step=1))
+        sum_days += day_of_selling[i]
+        init_route.append(Event(names['events'], day_start, day_of_selling[i]))
+        day_start += 1 + day_of_selling[i]
+
 else:
     pass
 
-search_btn = st.sidebar.button("Search!")
-if search_btn:
-    print_solution(init_route)
+show_agenda = st.sidebar.checkbox("Show agenda")
+search = st.sidebar.button("Search!")
+
+if show_agenda:
+    start.empty()
+    img = Image.open('../images/agenda.png')
+    st.subheader('Agenda')
+    st.image(img, width=600)
+
+if search:
+    st.subheader('Your initial solution is: ')
+    SolutionApp.print_solution(init_route)
+    a = st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+        unsafe_allow_html=True,
+    )
+    final_solution = SolutionApp(init_route, T_init, params['alpha'], T_stop, SolMethod.GEO, chain_len, init_route, 0)
+    final_solution.sym_ann_algorithm()
+    a.empty()
 else:
     pass
